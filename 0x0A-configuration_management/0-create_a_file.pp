4b4e0cd8c70ec606141895d0a03fa837e3db4bb9
abeni-al7 <abenezeralebachew3@gmail.com>
2024-03-25 21:17:44 +0300

    Puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
+# A puppet script that creates a file
 file {'/tmp/school':
-  path       => '/tmp/school',
-  mode       => '0744',
-  owner      => 'www-data',
-  group      => 'www-data',
-  content    => 'I love Puppet',
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
