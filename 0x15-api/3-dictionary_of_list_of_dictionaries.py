035391f6fe4495c5b4290c0cbd885c9d76be6751
abeni-al7 <abenezeralebachew3@gmail.com>
2024-04-22 20:42:58 +0300

    New

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -5,21 +5,28 @@ It then exports the data in the JSON format. recording all tasks from
 all employees to a file named todo_all_employees.json."""
 import json
 import requests
+from sys import argv
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    users = requests.get(url + "users").json()
+    url = "https://jsonplaceholder.typicode.com/users"
+    response = requests.get(url)
+    users = response.json()
     data = {}
     for user in users:
+        user_id = user.get("id")
+        name = user.get("username")
+        url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+            user_id)
+        response = requests.get(url)
+        todos = response.json()
         tasks = []
-        todos = requests.get(url + "todos?userId={}".format(
-            user.get("id"))).json()
-        for todo in todos:
-            tasks.append({"username": user.get("username"),
-                          "task": todo.get("title"),
-                          "completed": todo.get("completed")})
-        data[user.get("id")] = tasks
-
+        for task in todos:
+            task_dict = {}
+            task_dict["task"] = task.get("title")
+            task_dict["completed"] = task.get("completed")
+            task_dict["username"] = name
+            tasks.append(task_dict)
+        data[user_id] = tasks
 
     with open("todo_all_employees.json", "w") as jsonfile:
         json.dump(data, jsonfile)
