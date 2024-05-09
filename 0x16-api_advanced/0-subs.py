eb2b045cda9f2bbc9fcf96f4ba0ab0584fbf13fd
abeni-al7 <abenezeralebachew3@gmail.com>
2024-05-09 03:08:35 +0300

    Subs count

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,8 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers of a subreddit"""
-
+    if not subreddit or type(subreddit) is not str:
+        return 0
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
