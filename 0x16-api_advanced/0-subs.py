49ca2584e1e229e7ab521fbd49c926b3b3dd1ce2
abeni-al7 <abenezeralebachew3@gmail.com>
2024-05-09 03:11:35 +0300

    Subs count

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,10 +10,10 @@ def number_of_subscribers(subreddit):
         return 0
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False).json()
 
     if response.status_code == 200:
-        data = response.json().get('data')
+        data = response.json().get('data', {})
         if data:
-            return data.get('subscribers')
+            return data.get('subscribers', 0)
     return 0
