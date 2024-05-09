39b8aa0d41963ccb7f6dae2aa8a0e80d36d7f581
abeni-al7 <abenezeralebachew3@gmail.com>
2024-05-09 03:32:57 +0300

    Correct jsonization twics\e

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,5 +11,5 @@ def number_of_subscribers(subreddit):
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False).json()
-    data = response.json().get('data', {}).get('subscribers', 0)
+    data = response.get('data', {}).get('subscribers', 0)
     return data
